ac4e760785ac2b2a2da1df3d3a00dd862031881c
nastya <nastyaev4uk@gmail.com>
2025-08-05 22:59:49 +0300

    Lesson 1

---
+++ lesson_01/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = 'Anastasiia'
+print(my_name)
---
+++ test_lessons/lesson1.py
@@ -0,0 +1,30 @@
+#Задание 1
+my_height = 165
+print(my_height)
+
+#Задание 2
+my_name = "Anastasiia"
+my_name = "Anastasiia Evchuk"
+print(my_name)
+
+#Задание 3
+pet_name = input("Как зоут вашего питомца?")
+print("Ваш любимчик - " + pet_name)
+
+#Задание 4
+def print_python():
+    print("Учу Python!")
+
+print_python()
+
+#Задание 5
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
